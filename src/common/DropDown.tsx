d6651a5ebe5d02dcf37ce166a79d07a52bef40dd
Zajjaj Bin Sana <zajjaj.sana@paysyslabs.com>
2024-11-26 18:52:49 +0500

    Drop-down added in Registation page

---
+++ src/common/DropDown.tsx
@@ -0,0 +1,115 @@
+import {StyleSheet, ViewProps, ViewStyle, TextStyle} from 'react-native';
+import React, {ReactNode} from 'react';
+import DropDownPicker from 'react-native-dropdown-picker';
+
+type TDropDownProps = {
+  open: boolean;
+  setOpen: any;
+  items: Array<{label: string; value: any}>;
+  placeholder?: string;
+  style?: ViewStyle;
+  containerStyle?: ViewStyle;
+  itemStyle?: ViewStyle;
+  labelStyle?: TextStyle;
+  arrowStyle?: ViewStyle;
+  dropDownStyle?: ViewStyle;
+  dropDownContainerStyle?: ViewStyle;
+  defaultValue?: any;
+  setDefaultValue: any;
+  multiple?: boolean;
+  multipleText?: string;
+  searchable?: boolean;
+  searchablePlaceholder?: string;
+  searchablePlaceholderTextColor?: string;
+  selectedLabelStyle?: TextStyle;
+  selectedItemStyle?: ViewStyle;
+  selectedItemsStyle?: ViewStyle;
+  itemSeparatorStyle?: ViewStyle;
+  listStyle?: ViewStyle;
+};
+export const DropDown: React.FC<TDropDownProps> = ({
+  open,
+  items,
+  defaultValue,
+  setOpen,
+  setDefaultValue,
+}) => {
+  return (
+    <DropDownPicker
+      open={open}
+      value={defaultValue}
+      items={items}
+      placeholder="Select"
+      containerStyle={open ? styles.dropDownContOpen : styles.dropDownContClose}
+      style={styles.dropDownStyle}
+      dropDownContainerStyle={styles.dropDownContainerStyle}
+      arrowIconStyle={styles.arrowIconStyle}
+      labelStyle={styles.labelStyle}
+      setOpen={setOpen}
+      setValue={setDefaultValue}
+      placeholderStyle={styles.placeholderStyle}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  dropDownContClose: {
+    borderRadius: 25,
+    width: '100%',
+    height: 60,
+    backgroundColor: '#2A2A2A',
+    borderWidth: 1,
+    justifyContent: 'center',
+    tintColor: '#FFFFFF',
+  },
+
+  dropDownContOpen: {
+    borderRadius: 25,
+    borderBottomStartRadius: 0,
+    borderBottomEndRadius: 0,
+    width: '100%',
+    height: 60,
+    backgroundColor: '#2A2A2A',
+    justifyContent: 'center',
+  },
+
+  arrowIconStyle: {
+    width: 20,
+    height: 20,
+    marginBottom: 0,
+    tintColor: '#FFFFFF',
+  },
+
+  dropDownStyle: {
+    borderRadius: 25,
+    borderWidth: 0,
+    backgroundColor: '#2A2A2A',
+    height: 60,
+    justifyContent: 'center',
+    paddingHorizontal: 16,
+    tintColor: '#FFFFFF',
+  },
+
+  dropDownContainerStyle: {
+    borderWidth: 0,
+    borderBottomStartRadius: 25,
+    borderBottomEndRadius: 25,
+    width: 370,
+    backgroundColor: '#FFFFFF',
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 3},
+    shadowOpacity: 0.3,
+    shadowRadius: 5,
+    elevation: 5, // Elevation for Android
+  },
+  labelStyle: {
+    color: '#FFFFFF', // Make the dropdown item labels white
+    fontSize: 16,
+    fontWeight: 'bold',
+    tintColor: '#FFFFFF',
+  },
+  placeholderStyle: {
+    color: '#FFFFFF', // White placeholder text
+    fontSize: 16,
+  },
+});
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,12 +1,27 @@
 import {Pressable, StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import {TRegisterProps} from './types';
 import {IoChevronBackOutline} from 'react-icons/io5';
 import Input from '../../components/Input';
 import {SvgWrapper} from '../../common/SvgWrapper';
 import {goBackWhiteSvg, PersonSvg, WhiteArrowSvg} from '../../assets/svgs';
+import DropDownPicker from 'react-native-dropdown-picker';
+import {DropDown} from '../../common/DropDown';
 
 export const Register: React.FC<TRegisterProps> = ({navigation, route}) => {
+  const [selectVehicle, setSelectVehicle] = useState(false);
+  const [selectVehicle, setSelectVehicle] = useState(false);
+  const [selectValue, setSelectValue] = useState(null);
+  const [selectItems, setSelectItems] = useState([
+    {label: 'Apple', value: 'apple'},
+    {label: 'Banana', value: 'banana'},
+  ]);
+  const [CompanyValue, setCompanyValue] = useState(null);
+  const [companyItems, setCompanySelectItems] = useState([
+    {label: 'Apple', value: 'apple'},
+    {label: 'Banana', value: 'banana'},
+  ]);
+
   return (
     <View style={styles.container}>
       <Pressable style={{marginLeft: 10}} onPress={() => navigation.goBack()}>
@@ -16,17 +31,40 @@ export const Register: React.FC<TRegisterProps> = ({navigation, route}) => {
       <Text style={styles.mainHeading}>Register With Us</Text>
       <View style={styles.formContainer}>
         <View style={styles.feildContainer}>
-          <Text className='text-white m-2 text-base font-semibold	'>Name</Text>
-          <Input
-            placeholder="Enter Name"
+          <Text className="text-white m-2 text-base font-semibold	">Name</Text>
+          <Input placeholder="Enter Name" />
+        </View>
+
+        <View>
+          <Text className="text-white m-2 text-base font-semibold	">
+            Mobile Number
+          </Text>
+          <Input placeholder="Mobile Number" keyboardType="numeric" />
+        </View>
+
+        <View>
+          <Text className="text-white m-2 text-base font-semibold	">
+            Vehice Type
+          </Text>
+          <DropDown
+            open={selectVehicle}
+            items={selectItems}
+            defaultValue={selectValue}
+            setOpen={setSelectVehicle}
+            setDefaultValue={setSelectValue}
           />
         </View>
 
         <View>
-          <Text className='text-white m-2 text-base font-semibold	'>Mobile Number</Text>
-          <Input
-            placeholder="Mobile Number"
-            keyboardType='numeric'
+          <Text className="text-white m-2 text-base font-semibold	">
+            Individual / Company
+          </Text>
+          <DropDown
+            open={open}
+            items={companyItems}
+            defaultValue={CompanyValue}
+            setOpen={setOpen}
+            setDefaultValue={setCompanyValue}
           />
         </View>
       </View>
@@ -50,7 +88,8 @@ const styles = StyleSheet.create({
     marginHorizontal: '5%',
     alignItems: 'center',
   },
-  feildContainer:{
-    margin: 2
-  }
+  feildContainer: {
+    marginVertical: 8,
+    marginHorizontal: 2,
+  },
 });
