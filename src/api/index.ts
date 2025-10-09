a00d64bf92b6ba79a07d8395f188db5428537dd9
syedmaaz9905 <hivefivetechnologies@gmail.com>
2025-10-09 12:12:42 +0500

    updated baseurl

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 // export const BACKEND_URL = 'https://b01a5324e3c1.ngrok-free.app';
-export const BACKEND_URL = 'http://mrsteamer-backend-env.eba-2gwmyuuc.me-central-1.elasticbeanstalk.com';
+// export const BACKEND_URL = 'http://mrsteamer-backend-env.eba-2gwmyuuc.me-central-1.elasticbeanstalk.com';
+export const BACKEND_URL = 'https://api.mistersteamer.com';
 // export const BACKEND_URL = 'http://localhost:3001';
 // export const BACKEND_URL = 'http://10.0.2.2:3000/v1';
 
