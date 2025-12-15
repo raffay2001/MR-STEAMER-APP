5a6c9c36298de296ea8345ce38da725cbf7fb07c
syedmaaz9905 <hivefivetechnologies@gmail.com>
2025-12-15 18:29:15 +0500

    updated about us page

--- src/screens/AboutUs/index.tsx
+++ src/screens/AboutUs/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { SafeAreaView, View, Text } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import i18n from '../../i18n';
 
 const AboutUs: React.FC = () => {
+    const { t } = useTranslation();
+    const isAr = i18n.language?.startsWith('ar');
+
     return (
         <SafeAreaView style={{ flex: 1, backgroundColor: '#fff' }}>
             <View style={{ padding: 16 }}>
-                {/* <Text style={{ fontSize: 22, fontWeight: '700', color: '#111' }}>
-                    About Us
-                </Text> */}
-                <Text className='text-black opacity-50 text-[16px] font-normal leading-[29px] tracking-[1.5px]'>
-                    We value your time and therefore washing your car at our car wash will take no more than 15 minutes. Our company employs only highly qualified specialists, we use high-quality cleaning products and equipment, as well as premium materials. We value your time and therefore washing your car at our car wash will take no more than 15 minutes. Our company employs only highly qualified specialists, we use high-quality cleaning products and equipment, as well as premium materials.
+                <Text
+                    className="text-black opacity-50 text-[16px] font-normal leading-[29px] tracking-[1.5px]"
+                    style={{ textAlign: isAr ? 'right' : 'left' }}
+                >
+                    {t('about.body')}
                 </Text>
             </View>
         </SafeAreaView>
